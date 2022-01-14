bf3f66e1e5078253ed40cb615b404a33a4457ebf
yerlantemir <temirerlan99@gmail.com>
2022-01-14 17:13:17 +0600

    feat: easier copying, cypress adoption

--- src/components/ResultSuggestion.js
+++ src/components/ResultSuggestion.js
@@ -1,6 +1,6 @@
-import React from 'react';
+/* global chrome */
+import React, { useEffect, useState } from 'react';
 import { messages } from '../constants';
-import CopyButton from './CopyButton';
 import { ShieldIcon } from '@primer/octicons-react';
 
 const colors = ['bg-blue-600', 'bg-yellow-600', 'bg-orange-600', 'bg-red-600'];
@@ -8,6 +8,8 @@ const colors = ['bg-blue-600', 'bg-yellow-600', 'bg-orange-600', 'bg-red-600'];
 function Code({ children }) {
   return <span className="font-bold font-mono">{children}</span>;
 }
+
+const IS_DEVTOOL = !!(window.chrome && chrome.runtime && chrome.runtime.id);
 
 const levels = {
   Role: 0,
@@ -24,6 +26,17 @@ const levels = {
 function ResultSuggestion({ queries, result, dispatch }) {
   const suggestion = Object.values(queries).find(Boolean);
   const used = result?.expression || {};
+
+  const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => {
+        setCopied(false);
+      }, 1500);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [copied]);
 
   if (!suggestion) {
     return `Hmpf. I'm afraid I don't have any suggestions for you.`;
@@ -99,31 +112,63 @@ function ResultSuggestion({ queries, result, dispatch }) {
     message = <p>This is great. Ship it!</p>;
   }
 
+  async function attemptCopyToClipboard(suggestion) {
+    try {
+      if (!IS_DEVTOOL && 'clipboard' in navigator) {
+        await navigator.clipboard.writeText(suggestion);
+        return true;
+      }
+
+      const input = Object.assign(document.createElement('input'), {
+        type: 'text',
+        value: suggestion,
+      });
+
+      document.body.append(input);
+      input.select();
+      document.execCommand('copy');
+      input.remove();
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+  async function handleCopyClick() {
+    let text = suggestion.snippet;
+    let textToCopy = text;
+    if (typeof text === 'function') {
+      textToCopy = text();
+    }
+    textToCopy = textToCopy.replace('screen.get', 'cy.find');
+    const wasSuccessfullyCopied = await attemptCopyToClipboard(textToCopy);
+    if (wasSuccessfullyCopied) {
+      setCopied(true);
+    }
+    dispatch({
+      type: 'SET_QUERY',
+      query: suggestion.snippet,
+    });
+  }
+
   return (
     <div className="space-y-4 text-sm">
-      <div className={['text-white p-4 rounded space-y-2', color].join(' ')}>
+      <div
+        className={['text-white p-4 rounded space-y-2', color].join(' ')}
+        onClick={handleCopyClick}
+        style={{ cursor: 'pointer' }}
+      >
         <div className="font-bold text-xs">suggested query</div>
         {suggestion.excerpt && (
           <div className="flex justify-between">
-            <div
-              className="font-mono cursor-pointer text-xs"
-              onClick={() =>
-                dispatch({
-                  type: 'SET_QUERY',
-                  query: suggestion.snippet,
-                })
-              }
-            >
+            <div className="font-mono cursor-pointer text-xs">
               &gt; {suggestion.excerpt}
               <br />
             </div>
-            <CopyButton
-              title="copy query"
-              text={suggestion.snippet}
-              variant="white"
-            />
           </div>
         )}
+        {copied && <div style={{ fontSize: 30, color: 'red' }}>COPIED!!</div>}
       </div>
       <div className="min-h-8">{message}</div>
       {suggestion.warning && (
